bdcc51ea2b2082733727b1ec5ddc5d1ab61253c2
keisuzuki <kei.nishimura0131@gmail.com>
2024-02-09 13:37:56 +0900

    i18n_updated

--- app/controllers/incomes_expenses_controller.rb
+++ app/controllers/incomes_expenses_controller.rb
@@ -26,7 +26,7 @@ class IncomesExpensesController < ApplicationController
 
     respond_to do |format|
       if @incomes_expense.save
-        format.html { redirect_to incomes_expense_url(@incomes_expense), notice: "Incomes expense was successfully created." }
+        format.html { redirect_to incomes_expense_url(@incomes_expense), notice: t('shared_book.incomes_expenses_created') }
         format.json { render :show, status: :created, location: @incomes_expense }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -39,7 +39,7 @@ class IncomesExpensesController < ApplicationController
   def update
     respond_to do |format|
       if @incomes_expense.update(incomes_expense_params)
-        format.html { redirect_to incomes_expense_url(@incomes_expense), notice: "Incomes expense was successfully updated." }
+        format.html { redirect_to incomes_expense_url(@incomes_expense), notice: t('shared_book.incomes_expenses_updated') }
         format.json { render :show, status: :ok, location: @incomes_expense }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -53,7 +53,7 @@ class IncomesExpensesController < ApplicationController
     @incomes_expense.destroy!
 
     respond_to do |format|
-      format.html { redirect_to incomes_expenses_url, notice: "Incomes expense was successfully destroyed." }
+      format.html { redirect_to incomes_expenses_url, notice: t('shared_book.incomes_expenses_destroyed') }
       format.json { head :no_content }
     end
   end
