1f5116e85f9a2c7f08a69dc5d3cc02c9efa6bc7b
keisuzuki <kei.nishimura0131@gmail.com>
2024-04-12 23:33:16 +0900

    seedデータ最終完成。パーセンテージを整数で表示

--- db/seeds.rb
+++ db/seeds.rb
@@ -65,43 +65,73 @@ user_probabilities = [0.3, 0.2, 0.1, 0.1, 0.1, 0.1, 0.05, 0.05]
 
 users = User.all
 
+# 仮のレコードのIDを保持する配列を初期化
+temporary_record_ids = []
+
 12.times do |n|
     date = Date.today - 1.year + n.months
     expense_record = ExpenseRecord.create!(
       year: date.year,
       month: date.month
     )
-    
+
+    # 当月の全ユーザーの合計収入金額を初期化
+    total_income_of_month = 0
+
     # 同じ月に2〜6人のユーザーをランダムに選択
     num_users = rand(2..6)
     selected_users = users.sample(num_users, random: Random.new(user_probabilities.hash))
-    
-    # 合計支出金額のランダムな設定
-    total_amount = rand(50000..200000)
 
-    # 当月の全ユーザーの合計収入金額を計算しつつ、各ユーザーの収入を設定
-    total_income_of_month = 0
     selected_users.each do |user|
-      # ランダムな収入金額を設定（15万円〜30万円）
-      income = rand(150000..300000)
-      total_income_of_month += income
+        # ランダムな収入金額を設定（15万円〜30万円）
+        income = rand(150000..300000)
+        total_income_of_month += income
+
+        temporary_record = ExpenseRecordsDetail.create!(
+            expense_record: expense_record,
+            user: user,
+            ratio: 0,
+            total_amount: 0,
+            burden_amount: 0,
+            difference: 0,
+            income: income
+          )
+        
+          # 仮のレコードのIDを配列に追加
+        temporary_record_ids << temporary_record.id
+    end
+
+    selected_users.each do |user|
+  
+        # 合計支出金額のランダムな設定
+        total_amount = rand(50000..200000)
+
+        # 各ユーザーの収入に対するratioを計算
+        user_income = ExpenseRecordsDetail.where(expense_record: expense_record, user: user).pluck(:income).first
+        user_ratio = user_income.to_f / total_income_of_month.to_f
       
-      # 各ユーザーの収入に対するratioを計算
-      user_ratio = income.to_f / total_income_of_month.to_f
+        # 負担金額を計算
+        burden_amount = (user_ratio * total_amount).round
+        
+        # 差額を計算
+        difference = total_amount - burden_amount
       
-      burden_amount = (total_amount * user_ratio).round
-      difference = total_amount - burden_amount
-      
-      ExpenseRecordsDetail.create!(
-        expense_record: expense_record,
-        user: user,
-        ratio: user_ratio,
-        total_amount: total_amount,
-        burden_amount: burden_amount,
-        difference: difference,
-        income: income
-      )
+        user_ratio = user_ratio*100.round
+
+        # 割り勘記録詳細を作成
+        ExpenseRecordsDetail.create!(
+          expense_record: expense_record,
+          user: user,
+          ratio: user_ratio,
+          total_amount: total_amount,
+          burden_amount: burden_amount,
+          difference: difference,
+          income: user_income
+        )
     end
     
+    # 仮のレコードを削除
+    ExpenseRecordsDetail.where(id: temporary_record_ids).destroy_all
+
     puts "ExpenseRecord #{expense_record.id} created for #{date.strftime('%Y-%m')} with #{num_users} users"
 end
