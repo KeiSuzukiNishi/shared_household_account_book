13f4ebe36781e6d95e762714d09235c30899ebf3
keisuzuki <kei.nishimura0131@gmail.com>
2024-04-13 00:05:58 +0900

    manifest.jsとapplication.jsを修正

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,9 @@
 //= link_tree ../images
 //= link_tree ../../javascript .js
+//= link_directory ../../javascript/vendor/bootstrap/js .js
 //= link_directory ../../javascript/vendor/chart .js
 //= link_directory ../../javascript/vendor/datatables .js
 //= link_directory ../../javascript/vendor/jquery .js
 //= link_directory ../../javascript/vendor/jquery-easing .js
+//= link_directory ../../javascript/demo .js
 //= link_tree ../builds
