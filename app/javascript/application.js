25e078443270489a28834f058b8941a6639f93c3
keisuzuki <kei.nishimura0131@gmail.com>
2024-04-15 22:50:15 +0900

    cndの追加試み

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
 // import 'jquery/dist/jquery';
-import 'bootstrap/dist/js/bootstrap';
-
 import "@hotwired/turbo-rails"
 import "controllers"
 import "chartkick"
@@ -13,22 +11,7 @@ import "bootstrap"
 
 import "jquery_ujs"
 import "turbolinks"
-import "bootstrap"
-import "sb-admin/metisMenu.min"
-import "sb-admin/raphael-2.1.0.min"
-import "sb-admin/morris"
-import "sb-admin/morris-data"
-import "sb-admin/sb-admin"
-
 import 'app/assets/stylesheets/sb-admin-2';
-import 'app/javascript/sb-admin-2/sb-admin-2';
 import 'app/javascript/sb-admin-2/sb-admin-2.min';
-import 'app/javascript/vendor/bootstrap/js/bootstrap.bundle.min.js';
-import 'app/javascript/vendor/chart/Chart.min';
-import 'app/javascript/vendor/datatables/jquery.dataTables.min';
-import 'app/javascript/vendor/datatables/dataTables.bootstrap4.min';
 import 'app/javascript/vendor/jquery/jquery.min';
 import 'app/javascript/vendor/jquery-easing/jquery.easing.min';
-import 'app/javascript/demo/chart-area-demo';
-import 'app/javascript/demo/chart-pie-demo';
-import 'app/javascript/demo/datatables-demo';
