8b28b2fed91f35633b49ef6c4ccbe4038397cacf
keisuzuki <kei.nishimura0131@gmail.com>
2024-04-17 14:09:41 +0900

    incomes_expenseモデルのmodel rspec完了

--- app/models/incomes_expense.rb
+++ app/models/incomes_expense.rb
@@ -13,14 +13,6 @@ class IncomesExpense < ApplicationRecord
     belongs_to :category
     belongs_to :user
 
-    # def start_date
-    #     starts_at || dealt_on
-    # end
-
-    # def end_date
-    #     ends_at || dealt_on
-    # end
-
     def self.total_amount_by_month(year, month)
         where("extract(year from dealt_on) = ? AND extract(month from dealt_on) = ?", year, month)
           .sum(:amount)
---
+++ spec/models/incomes_expense_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe '収支モデル機能', type: :model do
+  describe 'バリデーションのテスト' do
+    context '収支の日付が空の場合' do
+      it 'バリデーションに失敗する' do
+        incomes_expense = FactoryBot.build(:incomes_expense, dealt_on: '')
+        expect(incomes_expense).not_to be_valid
+      end
+    end
+  end
+
+  describe 'バリデーションのテスト' do
+    context '収支種類が空の場合' do
+      it 'バリデーションに失敗する' do
+        incomes_expense = FactoryBot.build(:incomes_expense, income_expense_type: '')
+        expect(incomes_expense).not_to be_valid
+      end
+    end
+  end
+
+  describe 'バリデーションのテスト' do
+    context '会社が空の場合' do
+      it 'バリデーションに失敗する' do
+        incomes_expense = FactoryBot.build(:incomes_expense, company: '')
+        expect(incomes_expense).not_to be_valid
+      end
+    end
+  end
+
+  describe 'バリデーションのテスト' do
+    context '金額が空の場合' do
+      it 'バリデーションに失敗する' do
+        incomes_expense = FactoryBot.build(:incomes_expense, amount: '')
+        expect(incomes_expense).not_to be_valid
+      end
+    end
+  end
+  
+  describe "scopeの確認テスト" do
+    before(:each) do
+      @category = FactoryBot.create(:category)
+    end
+  
+    describe "収支タイプの選択と表示" do
+      it "収入と支出それぞれ登録した内容で表示" do
+        expense = FactoryBot.create(:incomes_expense, income_expense_type: "支出", category: @category)
+        income = FactoryBot.create(:incomes_expense, income_expense_type: "収入", category: @category)
+        expect(IncomesExpense.expenses).to eq([expense])
+        expect(IncomesExpense.incomes).to eq([income])
+      end
+    end
+  end
+end
