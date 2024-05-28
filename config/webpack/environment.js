856feefc7b74f6f2f2a97e005f09f1e865cfd5c4
keisuzuki <kei.nishimura0131@gmail.com>
2024-05-28 23:35:19 +0900

    webpackのintallを再度行う

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,16 +1,3 @@
-const { environment } = require('@rails/webpacker');
+const { environment } = require('@rails/webpacker')
 
-const tailwindcss = require('tailwindcss');
-environment.loaders.get('sass').use.push({
-  loader: 'postcss-loader',
-  options: {
-    postcssOptions: {
-      plugins: [
-        tailwindcss('./path/to/your/tailwind.config.js'),
-        require('autoprefixer'),
-      ],
-    },
-  },
-});
-
-module.exports = environment;
+module.exports = environment
