1e8c0565f751b4f9285f5c86e0b46a916caa7c4b
keisuzuki <kei.nishimura0131@gmail.com>
2024-07-16 15:14:03 +0900

    jquery.minをcdnで取り込みに修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,20 +10,20 @@ set :log_level, :info   # 8
 set :branch, 'expense_records_final'
 # set :rbenv_version, '3.3.0'
 
-after 'deploy:published', 'deploy:seed'   # 9
+# after 'deploy:published', 'deploy:seed'   # 9
 after 'deploy:finished', 'deploy:restart'   # 10
 
 namespace :deploy do
-  desc 'Run seed'
-  task :seed do
-    on roles(:db) do
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:seed'
-        end
-      end
-    end
-  end
+  # desc 'Run seed'
+  # task :seed do
+  #   on roles(:db) do
+  #     with rails_env: fetch(:rails_env) do
+  #       within current_path do
+  #         execute :bundle, :exec, :rake, 'db:seed'
+  #       end
+  #     end
+  #   end
+  # end
   desc 'Restart application'
   task :restart do
     invoke 'unicorn:restart'
